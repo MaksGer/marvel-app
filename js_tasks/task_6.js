cb0a89e344dd333abc71f1fd890ba97cc3322d20
Maks Gerasimovich <gmaks1806@gmail.com>
2019-11-29 15:50:40 +0200

    Tasks 6 and 7 were added

---
+++ js_tasks/task_6.js
@@ -0,0 +1,54 @@
+function countFigureParam() {
+    const figure = prompt('Enter type of figure: circle, square, rectangle or triangle');
+    let area = 0;
+    let perimeter = 0;
+
+    switch (figure) {
+        case 'circle':
+            const radius = prompt('Enter radius of your circle');
+
+            area = (Math.PI * (radius ** 2)).toFixed(2);
+            perimeter = (2 * Math.PI * radius).toFixed(2);
+
+            break;
+
+        case 'square':
+            const side = prompt('Enter side of your square');
+
+            area = (side ** 2).toFixed(2);
+            perimeter = (side * 4).toFixed(2);
+
+            break;
+
+        case 'rectangle':
+            const sideA = parseInt(prompt('Enter side A of your rectangle'));
+            const sideB = parseInt(prompt('Enter side B of your rectangle'));
+
+            area = (sideA * sideB).toFixed(2);
+            perimeter = ((sideA + sideB) * 2).toFixed(2);
+
+            break;
+
+        case 'triangle':
+            const firstSide = parseInt(prompt('Enter  first side of your triangle'));
+            const secondSide = parseInt(prompt('Enter second side of your triangle'));
+            const thirdSide = parseInt(prompt('Enter third side of your triangle'));
+            const height = parseInt(prompt('Enter height of your triangle'));
+
+            perimeter = (firstSide + secondSide + thirdSide).toFixed(2);
+            area = ((height * firstSide) / 2).toFixed(2);
+
+            break;
+
+        default:
+            alert('Try again');
+
+            break;
+
+    }
+    alert(
+        'Area of your circle: ' + area + '\n' +
+        'Perimeter of your circle: ' + perimeter
+    );
+
+}
