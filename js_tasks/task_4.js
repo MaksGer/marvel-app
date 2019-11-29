725095624a5264f996d34ac0825ef24beaa9a475
Maks Gerasimovich <gmaks1806@gmail.com>
2019-11-29 13:13:01 +0200

    Task_4 was fixed

--- js_tasks/task_4.js
+++ js_tasks/task_4.js
@@ -3,16 +3,14 @@ function drawChess() {
     let isLineEven = false;
 
     for (let i = 1; i <= 8; i++) {
-        let item = 1;
-
         isLineEven = !!(i % 2);
-        for (; item <= 8; item++) {
-            str += (isLineEven) ? ((item % 2) ? ' ' : 'X') : ((item % 2) ? 'X' : ' ');
-            if (item === 8) {
-                console.log(str);
-                str = '';
-            }
+
+        for (let item = 1; item <= 8; item++) {
+            str += isLineEven ? (item % 2 ? ' ' : 'X') : (item % 2 ? 'X' : ' ');
         }
+
+        console.log(str);
+        str = '';
     }
 }
 
