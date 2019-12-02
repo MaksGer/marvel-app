cd62427e42992077784b3faa97be0c8f9cee3864
Maks Gerasimovich <gmaks1806@gmail.com>
2019-12-02 15:01:05 +0200

    Task 12 was fixed

--- js_tasks/task_12.js
+++ js_tasks/task_12.js
@@ -1,7 +1,6 @@
 function sumArguments (arg1, arg2) {
         if (arg2) {
             return arg1 + arg2;
-
         } else {
             return function (nextArgument) {
                 return arg1 + nextArgument;
