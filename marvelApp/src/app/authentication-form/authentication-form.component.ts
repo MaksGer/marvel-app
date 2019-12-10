e0bd826182fa491d50c6eae142923eb3ada88cc5
Maks Gerasimovich <gmaks1806@gmail.com>
2019-12-10 18:48:59 +0300

    Code style was fixed, css was re-wrote, autentification-form.component.ts was fixed

--- marvelApp/src/app/authentication-form/authentication-form.component.ts
+++ marvelApp/src/app/authentication-form/authentication-form.component.ts
@@ -1,68 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 
-
 @Component({
-  selector: 'app-authentication-form',
-  templateUrl: './authentication-form.component.html',
-  styleUrls: ['./authentication-form.component.css']
+	selector: 'app-authentication-form',
+	templateUrl: './authentication-form.component.html',
+	styleUrls: ['./authentication-form.component.css']
 })
+
 export class AuthenticationFormComponent implements OnInit {
+	isLoginExist: boolean = true;
+	isLoginAndPassValid: boolean = true;
+	formToSignUp: FormGroup;
+	formToLogIn: FormGroup;
 
-  isLoginExist: boolean = true;
-  isLoginAndPassValid: boolean = true;
-  formToSignUp: FormGroup;
-  formToLogIn: FormGroup;
+	ngOnInit() {
+		this.formToSignUp = new FormGroup({
+			name: new FormControl('', [
+				Validators.required,
+				Validators.minLength(2)
+			]),
+			password: new FormControl(null, [
+				Validators.required,
+				Validators.minLength(5)
+			]),
+			email: new FormControl('', [])
+		});
 
-  ngOnInit() {
-    this.formToSignUp = new FormGroup({
-      name: new FormControl('', [
-        Validators.required,
-        Validators.minLength(2)
-      ]),
-      password: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(5)
-      ]),
-      email: new FormControl('', [
-        Validators.email,
-        Validators.required
-      ])
-    });
+		this.formToLogIn = new FormGroup({
+			login: new FormControl('', [
+				Validators.required,
+				Validators.minLength(2)
+			]),
+			password: new FormControl('', [
+				Validators.required,
+				Validators.minLength(5)
+			])
+		});
+	}
 
-    this.formToLogIn = new FormGroup({
-      login: new FormControl('', [
-        Validators.required,
-        Validators.minLength(2)
-      ]),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(5)
-      ])
-    })
-  }
 
-  submitSignUp() {
-    localStorage.setItem(this.formToSignUp.value.name, JSON.stringify({
-      password: this.formToSignUp.value.password,
-      email: this.formToSignUp.value.email
-    }));
+	submitSignUp() {
+		localStorage.setItem(this.formToSignUp.value.email, JSON.stringify({
+			password: this.formToSignUp.value.password,
+			name: this.formToSignUp.value.name
+		}));
 
-    this.isLoginExist = true;
-  };
+		this.isLoginExist = true;
+		this.formToSignUp.reset();
+	};
 
-  submitLogIn() {
-    let returnObj = JSON.parse(localStorage.getItem(this.formToLogIn.value.login));
+	submitLogIn() {
+		let returnObj = JSON.parse(localStorage.getItem(this.formToLogIn.value.login));
 
-    switch (true) {
-      case !localStorage.getItem(this.formToLogIn.value.login):
-      case returnObj.password != this.formToLogIn.value.password:
-        this.isLoginAndPassValid = false;
+		switch (true) {
+			case !localStorage.getItem(this.formToLogIn.value.login):
+			case returnObj.password != this.formToLogIn.value.password:
+				this.isLoginAndPassValid = false;
+				break;
 
-        break;
+			case returnObj.password === this.formToLogIn.value.password:
+				this.isLoginAndPassValid = true;
+		}
 
-      case returnObj.password === this.formToLogIn.value.password:
-        this.isLoginAndPassValid = true;
-    }
-  };
+		this.formToLogIn.reset();
+	};
 }
