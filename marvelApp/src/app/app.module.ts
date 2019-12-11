491ec6351de3ca2ee00ff73aed5a196c24a27cdd
Maks Gerasimovich <gmaks1806@gmail.com>
2019-12-11 16:47:42 +0300

    Validation was fixed

--- marvelApp/src/app/app.module.ts
+++ marvelApp/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthenticationFormComponent } from './authentication-form/authentication-form.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialModule} from "./material/material.module";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MaterialModule } from "./material/material.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
 	declarations: [
--- marvelApp/src/app/authentication-form/authentication-form.component.ts
+++ marvelApp/src/app/authentication-form/authentication-form.component.ts
@@ -24,6 +24,7 @@ export class AuthenticationFormComponent implements OnInit {
 				Validators.minLength(5)
 			]),
 			email: new FormControl(null, [
+				Validators.required,
 				Validators.pattern('(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])')
 			])
 		});
@@ -31,7 +32,7 @@ export class AuthenticationFormComponent implements OnInit {
 		this.formToLogIn = new FormGroup({
 			login: new FormControl('', [
 				Validators.required,
-				Validators.minLength(2)
+				Validators.pattern('(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])')
 			]),
 			password: new FormControl('', [
 				Validators.required,
@@ -53,16 +54,7 @@ export class AuthenticationFormComponent implements OnInit {
 	submitLogIn() {
 		let returnObj = JSON.parse(localStorage.getItem(this.formToLogIn.value.login));
 
-		switch (true) {
-			case !localStorage.getItem(this.formToLogIn.value.login):
-			case returnObj.password != this.formToLogIn.value.password:
-				this.isLoginAndPassValid = false;
-				break;
-
-			case returnObj.password === this.formToLogIn.value.password:
-				this.isLoginAndPassValid = true;
-		}
-
+		this.isLoginAndPassValid = returnObj && returnObj.password === this.formToLogIn.value.password;
 		this.formToLogIn.reset();
 	};
 
