e6342403ab61a42861a2f222216c10fd962fb41c
Maks Gerasimovich <gmaks1806@gmail.com>
2019-12-11 17:00:15 +0300

    Routing was added

--- marvelApp/src/app/app.module.ts
+++ marvelApp/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthenticationFormComponent } from './authentication-form/authentication-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialModule} from "./material/material.module";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { MaterialModule } from "./material/material.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MainPageComponent } from './main-page/main-page.component';
 
 
