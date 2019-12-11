d48619607a804c23edb75a3d965960fdae54a1f5
mviarenich <mviarenich@exadel.com>
2019-12-11 17:59:08 +0300

    MARVEL-2: Code style fixes

--- marvelApp/src/app/app.module.ts
+++ marvelApp/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AuthenticationFormComponent } from './authentication-form/authentication-form.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MaterialModule } from "./material/material.module";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AuthenticationFormComponent} from './authentication-form/authentication-form.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {MaterialModule} from "./material/material.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatSnackBarModule} from "@angular/material";
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		AuthenticationFormComponent
+		AuthenticationFormComponent,
 	],
 	imports: [
 		BrowserModule,
@@ -18,9 +19,10 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 		BrowserAnimationsModule,
 		MaterialModule,
 		ReactiveFormsModule,
-		FormsModule
+		FormsModule,
+		MatSnackBarModule,
 	],
 	providers: [],
-	bootstrap: [AppComponent]
+	bootstrap: [AppComponent],
 })
 export class AppModule { }
--- marvelApp/src/app/authentication-form/authentication-form.component.ts
+++ marvelApp/src/app/authentication-form/authentication-form.component.ts
@@ -1,62 +1,67 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
 	selector: 'app-authentication-form',
 	templateUrl: './authentication-form.component.html',
-	styleUrls: ['./authentication-form.component.css']
+	styleUrls: ['./authentication-form.component.css'],
 })
 
 export class AuthenticationFormComponent implements OnInit {
-	isLoginExist: boolean = true;
-	isLoginAndPassValid: boolean = true;
+	isLoginExist = true;
 	formToSignUp: FormGroup;
 	formToLogIn: FormGroup;
+	emailPattern = '^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$';
+	constructor(private _snackBar: MatSnackBar) {}
 
 	ngOnInit() {
 		this.formToSignUp = new FormGroup({
-			name: new FormControl('', [
-				Validators.required,
-				Validators.minLength(2),
-			]),
-			password: new FormControl(null, [
-				Validators.required,
-				Validators.minLength(5)
-			]),
 			email: new FormControl(null, [
-				Validators.required,
-				Validators.pattern('(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])')
-			])
+				Validators.pattern(this.emailPattern),
+			]),
+			name: new FormControl('', []),
+			password: new FormControl(null, [
+				Validators.minLength(8),
+			]),
 		});
 
 		this.formToLogIn = new FormGroup({
 			login: new FormControl('', [
 				Validators.required,
-				Validators.pattern('(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])')
+				Validators.pattern(this.emailPattern),
 			]),
 			password: new FormControl('', [
 				Validators.required,
-				Validators.minLength(5)
-			])
+				Validators.minLength(8),
+			]),
 		});
-	};
+	}
 
 	submitSignUp() {
 		localStorage.setItem(this.formToSignUp.value.email, JSON.stringify({
 			password: this.formToSignUp.value.password,
-			name: this.formToSignUp.value.name
+			name: this.formToSignUp.value.name,
 		}));
 
 		this.isLoginExist = true;
 		this.formToSignUp.reset();
-	};
+	}
 
 	submitLogIn() {
-		let returnObj = JSON.parse(localStorage.getItem(this.formToLogIn.value.login));
+		const returnObj = JSON.parse(localStorage.getItem(this.formToLogIn.value.login));
 
-		this.isLoginAndPassValid = returnObj && returnObj.password === this.formToLogIn.value.password;
-		this.formToLogIn.reset();
-	};
+		if(returnObj && returnObj.password === this.formToLogIn.value.password) {
+			this.formToLogIn.reset();
+		} else {
+			this._snackBar.open('Please check your Login and Password and try again', 'Close', {
+				duration: 4000,
+				verticalPosition: 'top',
+				horizontalPosition: 'center',
+				panelClass: 'error-snack-bar',
+			});
+		}
+	}
 
 	changeForm(form: FormGroup) {
 		this.isLoginExist = !this.isLoginExist;
