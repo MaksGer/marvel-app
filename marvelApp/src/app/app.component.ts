085a8382e33c83e78ff0aa93d996c0c5eb60f9ff
Maks Gerasimovich <gmaks1806@gmail.com>
2019-12-17 17:10:26 +0300

    Code style was fixed, spiner for others page except login page was deleted

--- marvelApp/src/app/app.component.ts
+++ marvelApp/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {NavigationEnd, NavigationStart, Event, Router} from "@angular/router";
 
 @Component({
 	selector: 'app-root',
@@ -8,20 +7,4 @@ import {NavigationEnd, NavigationStart, Event, Router} from "@angular/router";
 })
 export class AppComponent {
 	title = 'marvelApp';
-	timeout;
-	routerChanged = true;
-	constructor(private router: Router) {
-		router.events.subscribe((event: Event) => {
-			if (event instanceof NavigationStart) {
-				this.routerChanged = true;
-			}
-
-			if (event instanceof NavigationEnd) {
-				this.timeout = setTimeout(() => {
-					clearTimeout(this.timeout);
-					this.routerChanged = false;
-				}, 700);
-			}
-		});
-	}
 }
