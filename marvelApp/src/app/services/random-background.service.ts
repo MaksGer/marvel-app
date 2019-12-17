feef0036ec75c3a970e2e568e739487ef6b5ef65
Maks Gerasimovich <gmaks1806@gmail.com>
2019-12-17 10:57:54 +0300

    Code style was fixed

--- marvelApp/src/app/services/random-background.service.ts
+++ marvelApp/src/app/services/random-background.service.ts
@@ -7,22 +7,22 @@ export class RandomBackgroundService {
 	resultOfRandom: number;
 	themes = {
 		1: {
-			'backGroundColor': '#070fd7',
+			'backgroundColor': '#070fd7',
 			'color': '#070fd7',
 			'background': 'bg1',
 		},
 		2: {
-			'backGroundColor': '#5e2396',
+			'backgroundColor': '#5e2396',
 			'color': '#5e2396',
 			'background': 'bg2',
 		},
 		3: {
-			'backGroundColor': '#bf6d1b',
+			'backgroundColor': '#bf6d1b',
 			'color': '#bf6d1b',
 			'background': 'bg3',
 		},
 		4: {
-			'backGroundColor': '#d42e1b',
+			'backgroundColor': '#d42e1b',
 			'color': '#d42e1b',
 			'background': 'bg4',
 		},
