b2fac657071fa15e5a17432f850f8352f31046a9
Maks Gerasimovich <gmaks1806@gmail.com>
2019-12-18 10:47:53 +0300

    Log out icon was created

--- marvelApp/src/app/header/header.component.ts
+++ marvelApp/src/app/header/header.component.ts
@@ -1,17 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-	constructor(private authService: AuthService) { }
+export class HeaderComponent {
+	constructor(private authService: AuthService,
+				private router: Router,
+				) { }
 
 	links = ['heroes', 'comics', 'creators', 'events', 'series', 'stories', 'favorites'];
 	activeLink: string;
 
-	ngOnInit() { }
-
+	logout() {
+	this.router.navigate(['/login']);
+	sessionStorage.clear();
+	}
 }
