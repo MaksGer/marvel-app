9fb5dce0fe7e2c406b8c1527fd23eeffd6efc230
Maks Gerasimovich <gmaks1806@gmail.com>
2019-12-18 16:56:24 +0300

    Spinner after log out was fixed

--- marvelApp/src/app/authentication-form/authentication-form.component.ts
+++ marvelApp/src/app/authentication-form/authentication-form.component.ts
@@ -35,7 +35,7 @@ export class AuthenticationFormComponent implements OnInit {
 				this.timeout = setTimeout(() => {
 					clearTimeout(this.timeout);
 					this.routerChanged = false;
-				}, 1000);
+				}, 700);
 			}
 		});
 	}
