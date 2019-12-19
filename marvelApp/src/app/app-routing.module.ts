0799aa05b47e10ec471bec1afc838e19a4750c97
Maks Gerasimovich <gmaks1806@gmail.com>
2019-12-19 15:39:27 +0300

    Import main-page was removed

--- marvelApp/src/app/app-routing.module.ts
+++ marvelApp/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {MainPageComponent} from "./main-page/main-page.component";
 import {AuthenticationFormComponent} from "./authentication-form/authentication-form.component";
 import {AuthGuard} from "./auth.guard";
 import {HeroesComponent} from "./heroes/heroes.component";
