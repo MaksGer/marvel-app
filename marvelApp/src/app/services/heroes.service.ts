c3d92082b5df909c8c817ddb07b59ef2842f1fe8
Maks Gerasimovich <gmaks1806@gmail.com>
2019-12-24 15:22:25 +0300

    Request URL was fixed, params were added

--- marvelApp/src/app/services/heroes.service.ts
+++ marvelApp/src/app/services/heroes.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 
 @Injectable({
@@ -7,16 +7,26 @@ import {Observable} from "rxjs";
 })
 
 export class HeroesService {
-	publicKey = 'apikey=261184743b3ca5f2464aa5f310961b29';
-	urlAPI = `https://gateway.marvel.com/v1/public/characters?`;
+	publicKey = '261184743b3ca5f2464aa5f310961b29';
+	urlAPI = `https://gateway.marvel.com/v1/public/characters`;
 
 	constructor(private http: HttpClient) {}
 
 	getHeroes(): Observable<any> {
-		return this.http.get(this.urlAPI + this.publicKey);
+		return this.http.get(this.urlAPI, {
+			params: new HttpParams().set('apikey', this.publicKey),
+		} );
 	}
 
 	getHeroesFromUserSearch(name: string): Observable<any> {
-		return this.http.get( this.urlAPI + `nameStartsWith=${name}&` + this.publicKey);
+		let params = new HttpParams();
+
+		params = params.append('nameStartsWith', name);
+		params = params.append('apikey', this.publicKey);
+
+		return this.http.get( this.urlAPI, {
+			params
+		} );
 	}
 }
+// return this.http.get( this.urlAPI + `nameStartsWith=${name}&` + this.publicKey);
